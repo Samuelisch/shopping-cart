62ab94b032a9cb4eaa9b73a09ad56c5f373968da
Samuelisch <samuelchanwk@hotmail.com>
2021-12-06 15:16:45 +0800

    added Header and Router components imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Footer from './components/Footer'
+import Router from './components/Router'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  :root,
+  body {
+    font-family: Helvetica;
+    font-size: 10px;
+    color: rgb(15, 15, 15);
+    margin: 0;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <GlobalStyle />
+      <Header />
+      <Router />
     </div>
   );
 }
