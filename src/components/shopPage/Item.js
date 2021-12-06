da5dbe2915f1c95d75c776009b27ffbf76bd7ca8
Samuelisch <samuelchanwk@hotmail.com>
2021-12-06 21:02:31 +0800

    added styled components to Item instance, space for image and description

--- src/components/shopPage/Item.js
+++ src/components/shopPage/Item.js
@@ -1,15 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Item = ({ imgSrc, title, price, clickHandler }) => {
+const StyledContainer = styled.div`
+  width: 225px;
+  height: 300px;
+  border: 1px solid black;
+  margin: 30px;
+`
+
+const StyledImageContainer = styled.div`
+  width: 100%;
+  height: 225px;
+  border: 1px solid red;
+  background: rgb(245, 245, 245)
+`
+
+const StyledDescription = styled.div`
+  margin: 10px 0;
+  .title {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  .price {
+    margin-top: 10px;
+    font-size: 1.2rem;
+  }
+`
+
+const Item = ({ imgSrc, title, price, clickHandler, id }) => {
   return (
-    <div onClick={clickHandler}>
-      <img src={imgSrc} alt={title} />
-      <div>
-        <div>{title}</div>
-        <div>{price}</div>
-      </div>
-    </div>
+    <StyledContainer onClick={(e) => clickHandler(e, id)}>
+      <StyledImageContainer>
+        <img src={imgSrc} alt={title} />
+      </StyledImageContainer>
+      <StyledDescription>
+        <div className="title">{title}</div>
+        <div className="price">{price}</div>
+      </StyledDescription>
+    </StyledContainer>
   )
 }
 
