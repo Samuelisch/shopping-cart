9a9309f0e3330b16395487a1763c891d29372791
Samuelisch <samuelchanwk@hotmail.com>
2021-12-06 21:03:50 +0800

    added template behaviour to clicks on item instances, and template items to test item grid

--- src/components/shopPage/Shop.js
+++ src/components/shopPage/Shop.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ItemGrid from './ItemGrid'
 
 const Shop = () => {
+  const [items, setItems] = useState([
+    {
+      id: 149078,
+      imgSrc: '#',
+      name: 'first item',
+      price: '$24.50'
+    },
+    {
+      id: 85093428,
+      imgSrc: '#',
+      name: 'second item',
+      price: '$8.25'
+    },
+    {
+      id: 534958,
+      imgSrc: '#',
+      name: 'third item',
+      price: '$10.20'
+    },
+    {
+      id: 542334534,
+      imgSrc: '#',
+      name: 'fourth item',
+      price: '$20.75'
+    }
+  ])
+
+  const handleItemClick = (e, uniqueId) => {
+    console.log(uniqueId)
+  }
+
   return (
-    <h1>Hello Shop</h1>
+    <ItemGrid items={items} clickHandler={handleItemClick} />
   )
 }
 
