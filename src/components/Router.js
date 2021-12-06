60cef4846e6a5802215aadf852f0639e5be9d0c0
Samuelisch <samuelchanwk@hotmail.com>
2021-12-06 15:16:23 +0800

    added Router component, where it imports Shop Home and Contact pages, exported to App.js

---
+++ src/components/Router.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from './shopPage/Shop';
+import Home from './homePage/Home';
+import Contact from './contactPage/Contact';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Shop />}></Route>
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
