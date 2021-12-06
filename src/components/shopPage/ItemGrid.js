9eafa78988cd4eb26372f2833dcdd64e77e73c82
Samuelisch <samuelchanwk@hotmail.com>
2021-12-06 21:03:02 +0800

    Added flex and wrapping to grid of items

--- src/components/shopPage/ItemGrid.js
+++ src/components/shopPage/ItemGrid.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import Item from './Item'
+import styled from 'styled-components'
+
+const StyledGrid = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`
 
 const ItemGrid = ({ items, clickHandler }) => {
+  const itemsMap = items.map(item => (
+    <Item
+      key={item.id}
+      id={item.id} 
+      imgSrc={item.imgSrc}
+      title={item.name}
+      price={item.price}
+      clickHandler={clickHandler}
+    />
+  ))
 
+  return (
+    <StyledGrid>
+      {itemsMap}
+    </StyledGrid>
+  )
 }
 
 export default ItemGrid
