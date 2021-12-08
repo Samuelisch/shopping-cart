5482d40eb042fc56267d31dc89425eab7a14fb55
Samuelisch <samuelchanwk@hotmail.com>
2021-12-08 20:22:53 +0800

    initialised bundler for Cart item instances

---
+++ src/components/cartTab/CartItems.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CartItems = (items, addToCart, removeFromCart) => {
+  
+
+  return (
+    <h1>hello</h1>
+  )
+}
+
+export default CartItems
