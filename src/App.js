9ef12f6afc373491a8185c37d20d8738bbe67665
Samuelisch <samuelchanwk@hotmail.com>
2021-12-08 20:17:06 +0800

    added state for cartContent and cart display, added cart component when cart display is true

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/header/Header'
 import Footer from './components/Footer'
 import Router from './components/Router'
+import Cart from './components/cartTab/Cart'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
@@ -16,11 +17,34 @@ const GlobalStyle = createGlobalStyle`
 `
 
 function App() {
+  const [cartContent, setCartContent] = useState([])
+  const [cartView, setCartView] = useState(true)
+
+  const cartViewHandler = () => {
+    setCartView(!cartView)
+  }
+
+  const addToCart = (item, num=1) => {
+    
+  }
+
+  const removeFromCart = (item) => {
+
+  }
+
   return (
     <div>
       <GlobalStyle />
-      <Header />
-      <Router />
+      <Header cartClick={cartViewHandler} />
+      <Router addToCart={addToCart} />
+      {true &&
+        <Cart 
+          exitCartHandler={cartViewHandler} 
+          carContent={cartContent}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        /> 
+      }
     </div>
   );
 }
