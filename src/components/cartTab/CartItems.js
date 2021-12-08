3acd70e42c1e81ed68006cc17fd7332cdcd29b3c
Samuelisch <samuelchanwk@hotmail.com>
2021-12-08 21:51:55 +0800

    configured ItemInstance attribuites

--- src/components/cartTab/CartItems.js
+++ src/components/cartTab/CartItems.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import ItemInstance from './ItemInstance'
 
-const CartItems = (items, addToCart, removeFromCart) => {
-  
+const CartItems = ({ content, addToCart, removeFromCart }) => {
+  const itemList = content.map(item => (
+    <ItemInstance 
+      key={item.id}
+      item={item}
+      addHandler={addToCart}
+      removeHandler={removeFromCart}
+    />
+  ))
 
   return (
-    <h1>hello</h1>
+    <div>
+      {itemList}
+    </div>
   )
 }
 
