30f4dd3e41b6ffc2c6d411b842623365ffd08f37
Samuelisch <samuelchanwk@hotmail.com>
2021-12-08 21:51:16 +0800

    spelling correction

--- src/App.js
+++ src/App.js
@@ -18,14 +18,33 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
   const [cartContent, setCartContent] = useState([])
-  const [cartView, setCartView] = useState(true)
+  const [cartView, setCartView] = useState(false)
 
   const cartViewHandler = () => {
     setCartView(!cartView)
   }
 
+  const addNumToCart = (item, num) => {
+    let newNum = item.quantity + num
+    if (newNum > 9) {
+      return
+    }
+    let newItem = Object.assign({}, item, {quantity: newNum})
+    setCartContent(cartContent.map(content => content.id !== item.id ? content : newItem))
+  }
+
   const addToCart = (item, num=1) => {
-    
+    let copyContent = [...cartContent]
+    let existingItem = copyContent.find(content => content.id === item.id)
+    let newItem = {}
+    if (!existingItem) {
+      newItem = Object.assign({}, item, {quantity: num})
+      copyContent.push(newItem)
+      setCartContent(copyContent)
+    } else {
+      addNumToCart(existingItem, num)
+    }
+    console.log(cartContent)
   }
 
   const removeFromCart = (item) => {
@@ -37,10 +56,10 @@ function App() {
       <GlobalStyle />
       <Header cartClick={cartViewHandler} />
       <Router addToCart={addToCart} />
-      {true &&
+      {cartView &&
         <Cart 
           exitCartHandler={cartViewHandler} 
-          carContent={cartContent}
+          cartContent={cartContent}
           addToCart={addToCart}
           removeFromCart={removeFromCart}
         /> 
