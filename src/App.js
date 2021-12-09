130e263c2d12a1967a2ee48187008cf0899aa6b0
Samuelisch <samuelchanwk@hotmail.com>
2021-12-09 09:59:21 +0800

    moved all state and functions to Router component, instead of top level App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Router from './components/Router'
 import { createGlobalStyle } from 'styled-components'
 
@@ -14,50 +14,11 @@ const GlobalStyle = createGlobalStyle`
 `
 
 function App() {
-  const [cartContent, setCartContent] = useState([])
-  const [cartView, setCartView] = useState(false)
-
-  const cartViewHandler = () => {
-    setCartView(!cartView)
-  }
-
-  const addNumToCart = (item, num) => {
-    let newNum = item.quantity + num
-    if (newNum > 9) {
-      return
-    }
-    let newItem = Object.assign({}, item, {quantity: newNum})
-    setCartContent(cartContent.map(content => content.id !== item.id ? content : newItem))
-  }
-
-  const addToCart = (item, num=1) => {
-    let copyContent = [...cartContent]
-    let existingItem = copyContent.find(content => content.id === item.id)
-    let newItem = {}
-    if (!existingItem) {
-      newItem = Object.assign({}, item, {quantity: num})
-      copyContent.push(newItem)
-      setCartContent(copyContent)
-    } else {
-      addNumToCart(existingItem, num)
-    }
-    console.log(cartContent)
-  }
-
-  const removeFromCart = (item) => {
-
-  }
 
   return (
     <div>
       <GlobalStyle />
-      <Router 
-        addToCart={addToCart}
-        cartViewHandler={cartViewHandler}
-        cartContent={cartContent}
-        removeFromCart={removeFromCart}
-        cartView={cartView}
-      />
+      <Router />
     </div>
   );
 }
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from './shopPage/Shop';
 import Home from './homePage/Home';
@@ -7,8 +7,41 @@ import Cart from './cartTab/Cart';
 import Header from './header/Header';
 import Footer from './Footer';
 
-const Router = (props) => {
-  const { addToCart, cartView, cartViewHandler, cartContent, removeFromCart } = props
+const Router = () => {
+  const [cartContent, setCartContent] = useState([])
+  const [cartView, setCartView] = useState(false)
+
+  const cartViewHandler = () => {
+    setCartView(!cartView)
+  }
+
+  const addNumToCart = (item, num) => {
+    let newNum = item.quantity + num
+    if (newNum > 9) {
+      return
+    }
+    let newItem = Object.assign({}, item, {quantity: newNum})
+    setCartContent(cartContent.map(content => content.id !== item.id ? content : newItem))
+  }
+
+  const addToCart = (item, num=1) => {
+    let copyContent = [...cartContent]
+    let existingItem = copyContent.find(content => content.id === item.id)
+    let newItem = {}
+    if (!existingItem) {
+      newItem = Object.assign({}, item, {quantity: num})
+      copyContent.push(newItem)
+      setCartContent(copyContent)
+    } else {
+      addNumToCart(existingItem, num)
+    }
+    console.log(cartContent)
+  }
+
+  const removeFromCart = (item) => {
+
+  }
+  
   return (
     <BrowserRouter>
       <Header cartClick={cartViewHandler} />
